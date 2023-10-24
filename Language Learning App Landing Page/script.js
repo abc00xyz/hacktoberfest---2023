80cf12861b8b698524561b82d828780e6c2a07fa
Jayesh Jain <103871719+Jayesh-JainX@users.noreply.github.com>
2023-10-24 10:48:57 +0530

    Hacktoberfest

---
+++ Language Learning App Landing Page/script.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code for any animations or interactivity here
+// For example, you can add a dropdown event listener to change the language details dynamically.
+const languageDropdown = document.getElementById("languageDropdown");
+
+languageDropdown.addEventListener("change", function() {
+    const selectedLanguage = languageDropdown.value;
+    // You can update the page content based on the selected language here.
+    // For example, display language-specific information or lessons.
+});
